c7d66c5d15f3f982cb3fe30d783b7be64b5d0524
Jason <git-admin@casjaysdev.com>
2021-11-25 23:25:00 -0500

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,47 @@
+## xfce4  
+  
+Xfce is a lightweight and modular desktop environment  
+  
+Automatic install/update:
+
+```shell
+bash -c "$(curl -LSs https://github.com/desktopmgr/xfce4/raw/main/install.sh)"
+```
+
+Manual install:
+  
+requires:
+
+Debian based:
+
+```shell
+apt install xfce4 geany firefox thunar xfce4-terminal sxhkd
+```  
+
+Fedora Based:
+
+```shell
+yum install xfce4 geany firefox thunar xfce4-terminal sxhkd
+```  
+
+Arch Based:
+
+```shell
+pacman -S xfce4 geany firefox thunar xfce4-terminal sxhkd
+```  
+
+MacOS:  
+
+```shell
+brew install
+```
+  
+```shell
+mv -fv "$HOME/.config/xfce4" "$HOME/.config/xfce4.bak"
+git clone https://github.com/desktopmgr/xfce4 "$HOME/.config/xfce4"
+```
+  
+<p align=center>
+  <a href="https://wiki.archlinux.org/index.php/xfce4" target="_blank" rel="noopener noreferrer">xfce4 wiki</a>  |  
+  <a href="http://www.xfce.org" target="_blank" rel="noopener noreferrer">xfce4 site</a>
+</p>  
---
+++ install.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="xfce4"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 020820212259-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
+# @License       : LICENSE.md
+# @ReadME        : README.md
+# @Copyright     : Copyright: (c) 2021 Jason Hempstead, CasjaysDev
+# @Created       : Monday, Feb 08, 2021 22:59 EST
+# @File          : xfce4
+# @Description   : Installer script for xfce4
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/desktopmgr/installer/raw/main/functions}"
+connect_test() { ping -c1 1.1.1.1 &>/dev/null || curl --disable -LSs --connect-timeout 3 --retry 0 --max-time 1 1.1.1.1 2>/dev/null | grep -e "HTTP/[0123456789]" | grep -q "200" -n1 &>/dev/null; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+elif connect_test; then
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the main function
+user_installdirs
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# OS Support: supported_os unsupported_oses
+supported_os linux
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Make sure the scripts repo is installed
+scripts_check
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Defaults
+APPNAME="${APPNAME:-xfce4}"
+APPDIR="$CONF/$APPNAME"
+INSTDIR="$CASJAYSDEVSHARE/$SCRIPTS_PREFIX/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-master}"
+REPO="${DESKTOPMGR:-https://github.com/desktopmgr}/$APPNAME"
+REPORAW="$REPO/raw/$REPO_BRANCH"
+APPVERSION="$(__appversion "$REPORAW/version.txt")"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup plugins
+PLUGNAMES=""
+PLUGDIR="${SHARE:-$HOME/.local/share}/$APPNAME"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the desktopmgr function
+desktopmgr_install
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script options IE: --help --version
+show_optvars "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Do not update
+#installer_noupdate "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Requires root - no point in continuing
+#sudoreq  # sudo required
+#sudorun  # sudo optional
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# initialize the installer
+desktopmgr_run_init
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end with a space
+APP="$APPNAME geany firefox thunar xfce4-terminal sxhkd xfce4-notifyd notify-send lxappearance qt5ct "
+PERL=""
+PYTH=""
+PIPS=""
+CPAN=""
+GEMS=""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install packages - useful for package that have the same name on all oses
+install_packages "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install required packages using file
+install_required "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for perl modules and install using system package manager
+install_perl "$PERL"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for python modules and install using system package manager
+install_python "$PYTH"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for pip binaries and install using python package manager
+install_pip "$PIPS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for cpan binaries and install using perl package manager
+install_cpan "$CPAN"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for ruby binaries and install using ruby package manager
+install_gem "$GEMS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Other dependencies
+dotfilesreq git misc
+dotfilesreqadmin
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ensure directories exist
+ensure_dirs
+ensure_perms
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Backup if needed
+if [ -d "$APPDIR" ]; then
+  execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
+fi
+# Main progam
+if am_i_online; then
+  if [ -d "$INSTDIR/.git" ]; then
+    execute "git_update $INSTDIR" "Updating $APPNAME configurations"
+  else
+    execute "git_clone $REPO $INSTDIR" "Installing $APPNAME ions"
+  fi
+  # exit on fail
+  failexitcode $? "Failed to download $REPO/$APPNAME to $INSTDIR"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Plugins
+if am_i_online; then
+  if [ "$PLUGNAMES" != "" ]; then
+    if [ -d "$PLUGDIR"/PLUREP/.git ]; then
+      execute "git_update $PLUGDIR/PLUGREP" "Updating plugin PLUGNAME"
+    else
+      execute
+      "git_clone PLUGINREPO $PLUGDIR/PLUGREP" "Installing plugin PLUGREP"
+    fi
+  fi
+  # exit on fail
+  failexitcode $? "Failed to download Plugin repo"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run post install scripts
+run_post_custom() {
+  if pidof xfce4-panel &>/dev/null; then xfce4-panel -s &>/dev/null && xfce4-panel -q &>/dev/null && sleep 3; fi
+  for d in "$APPDIR"/panel/launcher-*; do
+    rm_rf "$d"
+  done
+  cp_rf "$INSTDIR/local_share/." "$HOME/.local/share/xfce4/."
+}
+
+run_postinst() {
+  run_post_custom
+  desktopmgr_run_post
+  if [ -n "$MPDSERVER" ]; then
+    GETMPDSERVER="$(getent ahosts "$MPDSERVER" 2>/dev/null | head -n1 | awk '{print $1}')"
+  else
+    GETMPDSERVER="localhost"
+  fi
+  mpdhostserver="${GETMPDSERVER}"
+  replace "$APPDIR" "MPDSERVER_host" "$mpdhostserver"
+  replace "$APPDIR" "/home/jason" "$HOME"
+}
+#
+execute "run_postinst" "Running post install scripts"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+printf_question_timeout "$ICON_QUESTION Should I install the themes and icons?" "1" "answer" "-s"
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+  SUDO_PROMPT="$(printf "\n\t\t\033[1;31m")[sudo]$(printf "\033[1;36m") password for $(printf "\033[1;32m")%p: $(printf "\033[0m")"
+  printf_cyan "Getting root privileges"
+  if sudo -n true; then
+    printf_blue "Proccessing fonts/icons/themes"
+    execute "sudo fontmgr install --all" "Installing fonts"
+    execute "sudo iconmgr install N.I.B." "Installing icons"
+    execute "sudo thememgr install Arc-Pink-Dark" "Installing themes"
+  fi
+fi
+printf_question_timeout "$ICON_QUESTION Should I install grub customizations?" "1" "answer" "-s"
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+  SUDO_PROMPT="$(printf "\n\t\t\033[1;31m")[sudo]$(printf "\033[1;36m") password for $(printf "\033[1;32m")%p: $(printf "\033[0m")"
+  printf_cyan "Getting root privileges"
+  if sudo -n true; then
+    printf_blue "Proccessing customizations"
+    execute "sudo systemmgr install grub" "Installing system packages"
+  fi
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# create version file
+desktopmgr_install_version
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# exit
+run_exit
+# end
